5be7782d163c4ae9dba7e85307948d68c2217f17
Hiryuto <108036759+Hiryuto@users.noreply.github.com>
2023-05-12 07:55:44 +0000

    add file

---
+++ idea.md
@@ -0,0 +1,3 @@
+# 原案
+https://youtu.be/m1UZBZc7wTc
+# 
---
+++ sw.js
@@ -0,0 +1,29 @@
+// Cache name
+const CACHE_NAME = 'pwa-sample-caches-v1';
+// Cache targets
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/index.css',
+  './js/index.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        return response ? response : fetch(event.request);
+      })
+  );
+});
