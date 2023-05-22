4f41f52163ff0188e70f3e61adbb6e777842907b
Hiryuto <108036759+Hiryuto@users.noreply.github.com>
2023-05-22 15:44:35 +0900

    試行錯誤中

--- sw.js
+++ sw.js
@@ -1,29 +1,51 @@
 // Cache name
-const CACHE_NAME = 'pwa-sample-caches-v1';
+const CACHE_NAME = "pwa-sample-caches-v2.1.6";
 // Cache targets
-const urlsToCache = [
-  './',
-  './index.html',
-  './css/index.css',
-  './js/index.js'
-];
+const urlsToCache = ["/", "/index.html", "/css/index.css", "/js/index.js"];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener("install", function (event) {
+  return install(event);
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
+self.addEventListener("message", function (event) {
+  return install(event);
+});
+
+const install = (event) => {
+  return event.waitUntil(
     caches
-      .match(event.request)
-      .then((response) => {
-        return response ? response : fetch(event.request);
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        urlsToCache.map((url) => {
+          return fetch(new Request(url)).then((response) => {
+            return cache.put(url, response);
+          });
+        });
       })
+      .catch(function (err) {
+        console.log(err);
+      })
+  );
+};
+
+self.addEventListener("activate", function (event) {
+  // waitUntil()でイベントの完了を処理が成功するまで遅延させる
+  event.waitUntil(
+    // cacheStorageの中の全てのcacheを確認する
+    caches.keys().then(function (cacheKeys) {
+      return Promise.all(
+        cacheKeys
+          .filter(function (cacheKey) {
+            // キー名を確認してキャッシュ名とバージョンを確認する
+            const [cacheName, cacheVersion] = cacheKey.split(":");
+            // 同じキャッシュ名でバージョンが異なるものを削除対象とする
+            return cacheName == CACHE_NAME && cacheVersion != CACHE_VERSION;
+          })
+          .map(function (cacheKey) {
+            // 削除対象としたキーのcacheを全てcacheStorageから削除する
+            return caches.delete(cacheKey);
+          })
+      );
+    })
   );
 });
